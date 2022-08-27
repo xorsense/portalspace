142567a9aaea431c42014c849a5f4897a531cbff
magickibbles <abi.chr567@gmail.com>
2022-08-26 21:57:30 -0400

    cleaned up code, added hello name & number is

--- main.go
+++ main.go
@@ -4,19 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type LogMiddleware struct {
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("req: %#v", r)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (l *LogMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("req: %#v", r)
-	fmt.Fprint(w, "Hello, world!")
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+func NumProvider(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	num := vars["num"]
+	fmt.Fprintf(w, "Your number is: %s", num)
 }
 
 func main() {
-	lm := LogMiddleware{}
-	http.Handle("/", &lm)
+	r := mux.NewRouter()
+	r.HandleFunc("/hello/{name}", HelloWorld)
+	r.HandleFunc("/numis/{num}", NumProvider)
+	r.Use(LogMiddleware)
+	http.Handle("/", r)
 	log.Println("Server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
